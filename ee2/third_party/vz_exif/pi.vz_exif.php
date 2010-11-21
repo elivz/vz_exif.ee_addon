47e9ea92dc5957fef389dadbd5cbaeab8a283d36
Eli Van Zoeren <eli@elivz.com>
2010-11-20 17:39:50 -0800

    Re-indent code for consistency

--- ee2/third_party/vz_exif/pi.vz_exif.php
+++ ee2/third_party/vz_exif/pi.vz_exif.php
@@ -3,13 +3,13 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $plugin_info = array(
-  'pi_name' => 'VZ Exif',
-  'pi_version' => '1.0',
-  'pi_author' => 'Eli Van Zoeren',
-  'pi_author_url' => 'http://elivz.com/',
-  'pi_description' => 'Extract the Exif information from an image',
-  'pi_usage' => Vz_exif::usage()
-  );
+    'pi_name' => 'VZ Exif',
+    'pi_version' => '1.0',
+    'pi_author' => 'Eli Van Zoeren',
+    'pi_author_url' => 'http://elivz.com/',
+    'pi_description' => 'Extract the Exif information from an image',
+    'pi_usage' => Vz_exif::usage()
+);
 
 /**
  * Memberlist Class
@@ -18,65 +18,65 @@ $plugin_info = array(
  * @category		Plugin
  * @author			Eli Van Zoeren
  * @copyright		Copyright (c) 2010, Eli Van Zoeren
- * @link				http://elivz.com/blog/single/vz_exif
+ * @link			http://elivz.com/blog/single/vz_exif
  * @license			http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported/
  */
 
 class Vz_exif {
 	
-	/*
-	 * Tag pair: {exp:vz_exif}Template data{/exp:vz_exif}
-	 */
-  function Vz_exif()
-  {
-		$this->EE =& get_instance();
-  }
+    /*
+     * Tag pair: {exp:vz_exif}Template data{/exp:vz_exif}
+     */
+    function Vz_exif()
+    {
+        $this->EE =& get_instance();
+    }
 
-	function exif()
-	{
-		$tagdata = $this->EE->TMPL->tagdata;
-		
-		// Get all the data
-		$data = array(array(
-		  'size' => $this->get_exif('FileSize'),
-		  'height' => $this->get_exif('Height'),
-		  'width' => $this->get_exif('Width'),
-		  'date' => $this->get_exif('DateTime'),
-		  'make' => $this->get_exif('Make'),
-		  'model' => $this->get_exif('Model'),
-		  'focal_length' => $this->get_exif('FocalLength'),
-		  'focal_length_equiv' => $this->get_exif('FocalLengthIn35mmFilm'),
-		  'aperture' => $this->get_exif('ApertureFNumber'),
-		  'shutter' => $this->get_exif('ExposureTime'),
-		  'iso' => $this->get_exif('ISOSpeedRatings'),
-		  'software' => $this->get_exif('Software'),
-		  'flash' => $this->get_exif('Flash'),
-    ));
-    
-    // Run the conditional statements
-    $tagdata = $this->EE->functions->prep_conditionals($tagdata, $data[0]);
-    
-    // Replace the tags with their values
-		return $this->EE->TMPL->parse_variables($tagdata, $data);
-	}
+    function exif()
+    {
+        $tagdata = $this->EE->TMPL->tagdata;
+        
+        // Get all the data
+        $data = array(array(
+            'size' => $this->get_exif('FileSize'),
+            'height' => $this->get_exif('Height'),
+            'width' => $this->get_exif('Width'),
+            'date' => $this->get_exif('DateTime'),
+            'make' => $this->get_exif('Make'),
+            'model' => $this->get_exif('Model'),
+            'focal_length' => $this->get_exif('FocalLength'),
+            'focal_length_equiv' => $this->get_exif('FocalLengthIn35mmFilm'),
+            'aperture' => $this->get_exif('ApertureFNumber'),
+            'shutter' => $this->get_exif('ExposureTime'),
+            'iso' => $this->get_exif('ISOSpeedRatings'),
+            'software' => $this->get_exif('Software'),
+            'flash' => $this->get_exif('Flash'),
+        ));
+        
+        // Run the conditional statements
+        $tagdata = $this->EE->functions->prep_conditionals($tagdata, $data[0]);
+        
+        // Replace the tags with their values
+        return $this->EE->TMPL->parse_variables($tagdata, $data);
+    }
   
-  /*
-   * Catch any other calls and use the method name called`
-   * as the EXIF value to retrieve
-   */
-  function size() { return $this->get_exif('FileSize'); }
-	function height() { return $this->get_exif('Height'); }
-  function width() { return $this->get_exif('Width'); }
-  function date() { return $this->get_exif('DateTime'); }
-  function make() { return $this->get_exif('Make'); }
-  function model() { return $this->get_exif('Model'); }
-  function focal_length() { return $this->get_exif('FocalLength'); }
-  function focal_length_equiv() { return $this->get_exif('FocalLengthIn35mmFilm'); }
-  function aperture() { return $this->get_exif('ApertureFNumber'); }
-  function shutter() { return $this->get_exif('ExposureTime'); }
-  function iso() { return $this->get_exif('ISOSpeedRatings'); }
-  function software() { return $this->get_exif('Software'); }
-  function flash() { return $this->get_exif('Flash'); }
+    /*
+    * Catch any other calls and use the method name called`
+    * as the EXIF value to retrieve
+    */
+    function size() { return $this->get_exif('FileSize'); }
+    function height() { return $this->get_exif('Height'); }
+    function width() { return $this->get_exif('Width'); }
+    function date() { return $this->get_exif('DateTime'); }
+    function make() { return $this->get_exif('Make'); }
+    function model() { return $this->get_exif('Model'); }
+    function focal_length() { return $this->get_exif('FocalLength'); }
+    function focal_length_equiv() { return $this->get_exif('FocalLengthIn35mmFilm'); }
+    function aperture() { return $this->get_exif('ApertureFNumber'); }
+    function shutter() { return $this->get_exif('ExposureTime'); }
+    function iso() { return $this->get_exif('ISOSpeedRatings'); }
+    function software() { return $this->get_exif('Software'); }
+    function flash() { return $this->get_exif('Flash'); }
 
 	/*
 	 * This is the heart of the plugin.
@@ -85,86 +85,86 @@ class Vz_exif {
 	 */
 	private function get_exif($tag)
 	{
-    $image = $this->EE->TMPL->fetch_param('image');
-    $root = $this->EE->TMPL->fetch_param('path');
-		
-		// Initialize the cache
-    $cache =& $this->EE->session->cache['vz_exif'][$root.$image];
-		
-    // Only get the Exif data once per page load
-    if (!isset($cache))
+        $image = $this->EE->TMPL->fetch_param('image');
+        $root = $this->EE->TMPL->fetch_param('path');
+        	
+        // Initialize the cache
+        $cache =& $this->EE->session->cache['vz_exif'][$root.$image];
+        	
+        // Only get the Exif data once per page load
+        if (!isset($cache))
+        {
+            $image = str_ireplace('http://'.$_SERVER['HTTP_HOST'], '', $image);
+            $file = '';
+            	
+            if ($root)
+            {
+            	// Add the path to the image file to get the full path
+            	$file = $this->EE->functions->remove_double_slashes($root.$image);
+            }
+            elseif (strncmp($image, '/', 1) == 0)
+            {
+            	// The image url is relative to the web root
+            	$site_relative_url = trim(str_ireplace('http://'.$_SERVER['HTTP_HOST'], '', $this->EE->functions->fetch_site_index(1)), '/');
+            	$root_path = str_ireplace($site_relative_url, '', FCPATH);
+            	$file = $this->EE->functions->remove_double_slashes($root_path.$image);
+            }
+            
+            // Get the data from the file
+            if (@exif_imagetype($file) != IMAGETYPE_JPEG) return '<!-- The file "'.$image.'" could not be found or was not in jpeg format -->';
+            $cache = exif_read_data($file);
+    	}
+    
+    	$exif = $cache;
+    	
+    	// Get the value we need from the array
+    	switch ($tag) {
+    		case 'FileSize':
+    			return isset($exif[$tag]) ? round($exif[$tag] / 1024) : '';
+    		case 'Height': case 'Width': case 'ApertureFNumber':
+    			return isset($exif['COMPUTED'][$tag]) ? $exif['COMPUTED'][$tag] : '';
+    		case 'Make': case 'Model':
+    			return isset($exif[$tag]) ? ucwords(strtolower($exif[$tag])) : '';
+    		case 'FocalLength':
+    			$length = '';
+    			if (isset($exif[$tag])) eval('$length = '.$exif[$tag].';');
+    			return $length;
+    		case 'ExposureTime':
+    			$val = '';
+    			if (isset($exif[$tag]))
+    			{
+    				if (strstr($exif[$tag], '/'))
+    				{
+    					// Reduce the fraction
+    					$val_parts = explode('/', $exif[$tag]);
+    					$val = '1/' . ($val_parts[1] / $val_parts[0]);
+    				}
+    				elseif ($exif[$tag] < 1)
+    				{
+    					// Turn the decimal into a fraction
+    					$val = '1/' . (1 / $exif[$tag]);
+    				}
+    				else
+    				{
+    					$val = $exif[$tag];
+    				}
+    			}
+    			return $val;
+    		case 'DateTime':
+    			$format = $this->EE->TMPL->fetch_param('format');
+    			$date = strtotime(isset($exif['DateTimeOriginal']) ? $exif['DateTimeOriginal'] : $exif['DateTime']);
+    			return $format ? $this->EE->localize->decode_date($format, $date) : $date;
+    		case 'Flash':
+    			return !@empty($exif['Flash']) ? 'Yes' : '';
+    		default:
+    			return isset($exif[$tag]) ? $exif[$tag] : '';
+    	}
+    }
+
+    function usage()
     {
-			$image = str_ireplace('http://'.$_SERVER['HTTP_HOST'], '', $image);
-			$file = '';
-				
-			if ($root)
-			{
-				// Add the path to the image file to get the full path
-				$file = $this->EE->functions->remove_double_slashes($root.$image);
-			}
-			elseif (strncmp($image, '/', 1) == 0)
-			{
-				// The image url is relative to the web root
-				$site_relative_url = trim(str_ireplace('http://'.$_SERVER['HTTP_HOST'], '', $this->EE->functions->fetch_site_index(1)), '/');
-				$root_path = str_ireplace($site_relative_url, '', FCPATH);
-				$file = $this->EE->functions->remove_double_slashes($root_path.$image);
-			}
-			
-			// Get the data from the file
-			if (@exif_imagetype($file) != IMAGETYPE_JPEG) return '<!-- The file "'.$image.'" could not be found or was not in jpeg format -->';
-			$cache = exif_read_data($file);
-		}
-
-		$exif = $cache;
-		
-		// Get the value we need from the array
-		switch ($tag) {
-			case 'FileSize':
-				return isset($exif[$tag]) ? round($exif[$tag] / 1024) : '';
-			case 'Height': case 'Width': case 'ApertureFNumber':
-				return isset($exif['COMPUTED'][$tag]) ? $exif['COMPUTED'][$tag] : '';
-			case 'Make': case 'Model':
-				return isset($exif[$tag]) ? ucwords(strtolower($exif[$tag])) : '';
-			case 'FocalLength':
-				$length = '';
-				if (isset($exif[$tag])) eval('$length = '.$exif[$tag].';');
-				return $length;
-			case 'ExposureTime':
-				$val = '';
-				if (isset($exif[$tag]))
-				{
-					if (strstr($exif[$tag], '/'))
-					{
-						// Reduce the fraction
-						$val_parts = explode('/', $exif[$tag]);
-						$val = '1/' . ($val_parts[1] / $val_parts[0]);
-					}
-					elseif ($exif[$tag] < 1)
-					{
-						// Turn the decimal into a fraction
-						$val = '1/' . (1 / $exif[$tag]);
-					}
-					else
-					{
-						$val = $exif[$tag];
-					}
-				}
-				return $val;
-			case 'DateTime':
-				$format = $this->EE->TMPL->fetch_param('format');
-				$date = strtotime(isset($exif['DateTimeOriginal']) ? $exif['DateTimeOriginal'] : $exif['DateTime']);
-				return $format ? $this->EE->localize->decode_date($format, $date) : $date;
-			case 'Flash':
-				return !@empty($exif['Flash']) ? 'Yes' : '';
-			default:
-				return isset($exif[$tag]) ? $exif[$tag] : '';
-		}
-	}
-
-  function usage()
-  {
-	  ob_start(); 
-	  ?>
+        ob_start(); 
+        ?>
 The VZ Exif Plugin extracts Exif data from an image and makes it available in your templates.
 
 TAG PAIR:
@@ -204,11 +204,11 @@ This is the url of the image to get Exif data from. The image needs to be on you
 root= (optional)
 VZ Exif will do its best to determine the server path to the image you give it, but in some cases you may want to specify the root path manually. The root url will simply be prepended to the image url (minus the domain name, if there is one).
 
-	  <?php
-	  $buffer = ob_get_contents();
-	  ob_end_clean(); 
-	  return $buffer;
-  }
+        <?php
+        $buffer = ob_get_contents();
+        ob_end_clean(); 
+        return $buffer;
+    }
 
 }
 
