d7ee857410019c4056dc1d47f07f243ce852b08b
Eli Van Zoeren <eli@elivz.com>
2019-06-01 13:20:40 -0400

    Bump version number

--- vz_exif/addon.setup.php
+++ vz_exif/addon.setup.php
@@ -5,7 +5,7 @@ return array(
     'author_url'  => 'https://github.com/elivz',
     'name'        => 'VZ Exif',
     'description' => 'Extracts Exif data from an image and makes it available in your templates.',
-    'version'     => '2.0.0',
+    'version'     => '3.0.0',
     'namespace'   => 'elivz\Addons\VZExif',
     'settings_exist' => false,
     'plugin.typography' => false
