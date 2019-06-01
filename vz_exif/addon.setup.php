849ab02797535c62bd9ba03e3983e360e7fe0177
Eli Van Zoeren <eli@elivz.com>
2019-06-01 13:19:26 -0400

    Reformat code

--- vz_exif/addon.setup.php
+++ vz_exif/addon.setup.php
@@ -1,12 +1,12 @@
 <?php
 
 return array(
-	'author'      => 'Eli Van Zoeren',
-	'author_url'  => 'https://github.com/elivz',
-	'name'        => 'VZ Exif',
-	'description' => 'Extracts Exif data from an image and makes it available in your templates.',
-	'version'     => '2.0.0',
-	'namespace'   => 'elivz\Addons\VZExif',
-	'settings_exist' => FALSE,
-	'plugin.typography' => FALSE
+    'author'      => 'Eli Van Zoeren',
+    'author_url'  => 'https://github.com/elivz',
+    'name'        => 'VZ Exif',
+    'description' => 'Extracts Exif data from an image and makes it available in your templates.',
+    'version'     => '2.0.0',
+    'namespace'   => 'elivz\Addons\VZExif',
+    'settings_exist' => false,
+    'plugin.typography' => false
 );
--- vz_exif/pi.vz_exif.php
+++ vz_exif/pi.vz_exif.php
@@ -1,20 +1,20 @@
 <?php
 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-
+if (! defined('BASEPATH')) { 
+    exit('No direct script access allowed');
+}
 
 /**
- * @package         ExpressionEngine
- * @category        Plugin
- * @author          Eli Van Zoeren
- * @copyright       Copyright (c) 2014, Eli Van Zoeren
- * @link            https://github.com/elivz/vz_exif.ee_addon
- * @license         http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported/
+ * @package   ExpressionEngine
+ * @category  Plugin
+ * @author    Eli Van Zoeren
+ * @copyright Copyright (c) 2014, Eli Van Zoeren
+ * @link      https://github.com/elivz/vz_exif.ee_addon
+ * @license   http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported/
  */
 
-class Vz_exif {
-
+class Vz_exif
+{
     public $return_data;
 
     /*
@@ -59,70 +59,84 @@ class Vz_exif {
     * Catch any other calls and use the method name called`
     * as the EXIF value to retrieve
     */
-    public function size() {
+    public function size()
+    {
         return $this->get_exif('FileSize');
     }
 
-    public function height() {
+    public function height()
+    {
         return $this->get_exif('Height');
     }
 
-    public function width() {
+    public function width()
+    {
         return $this->get_exif('Width');
     }
 
-    public function date() {
+    public function date()
+    {
         return $this->get_exif('DateTime');
     }
 
-    public function make() {
+    public function make()
+    {
         return $this->get_exif('Make');
     }
 
-    public function model() {
+    public function model()
+    {
         return $this->get_exif('Model');
     }
 
-    public function focal_length() {
+    public function focal_length()
+    {
         return $this->get_exif('FocalLength');
     }
 
-    public function focal_length_equiv() {
+    public function focal_length_equiv()
+    {
         return $this->get_exif('FocalLengthIn35mmFilm');
     }
 
-    public function aperture() {
+    public function aperture()
+    {
         return $this->get_exif('ApertureFNumber');
     }
 
-    public function shutter() {
+    public function shutter()
+    {
         return $this->get_exif('ExposureTime');
     }
 
-    public function iso() {
+    public function iso()
+    {
         return $this->get_exif('ISOSpeedRatings');
     }
 
-    public function software() {
+    public function software()
+    {
         return $this->get_exif('Software');
     }
 
-    public function flash() {
+    public function flash()
+    {
         return $this->get_exif('Flash');
     }
 
-    public function latitude() {
+    public function latitude()
+    {
         return $this->get_exif('GPSLatitude');
     }
 
-    public function longitude() {
+    public function longitude()
+    {
         return $this->get_exif('GPSLongitude');
     }
 
 
     /*
-     * This is the heart of the plugin.
-     * Get the exif data from the image
+     * This is the heart of the plugin. Get the exif data from the image
      * and return in in an array
      */
     private function get_exif($tag)
@@ -138,24 +152,22 @@ class Vz_exif {
 
 
         // Only get the Exif data once per page load
-        if (!isset($cache))
-        {
+        if (!isset($cache)) {
             $image = str_ireplace('http://'.$_SERVER['HTTP_HOST'], '', $image);
             $file = '';
 
-            if ($root)
-            {
+            if ($root) {
                 // Add the path to the image file to get the full path
                 $file = reduce_double_slashes($root.$image);
             }
-            elseif (strncmp($image, '/', 1) == 0)
-            {
+            elseif (strncmp($image, '/', 1) == 0) {
                 // The image url is relative to the web root
                 $file = reduce_double_slashes(FCPATH.'/'.$image);
             }
 
             // Get the data from the file
-            if (@exif_imagetype($file) != IMAGETYPE_JPEG) return '<!-- The file "'.$file.'" could not be found or was not in jpeg format -->';
+            if (@exif_imagetype($file) != IMAGETYPE_JPEG) { return '<!-- The file "'.$file.'" could not be found or was not in jpeg format -->';
+            }
             $cache = exif_read_data($file);
         }
 
@@ -163,79 +175,70 @@ class Vz_exif {
 
         // Get the value we need from the array
         switch ($tag) {
-            case 'FileSize':
-                return isset($exif[$tag]) ? round($exif[$tag] / 1024) : '';
-            case 'Height': case 'Width': case 'ApertureFNumber':
-                return isset($exif['COMPUTED'][$tag]) ? $exif['COMPUTED'][$tag] : '';
-            case 'Make': case 'Model':
-                return isset($exif[$tag]) ? ucwords(strtolower($exif[$tag])) : '';
-            case 'FocalLength':
-                $length = '';
-                if (isset($exif[$tag]))
-                {
-                    $length = $exif[$tag];
-                    if (strpos($exif[$tag], '/'))
-                    {
-                        $parts = explode('/', $length);
-                        if (count($parts) == 2)
-                        {
-                            $length = $parts[0] / $parts[1];
-                        }
+        case 'FileSize':
+            return isset($exif[$tag]) ? round($exif[$tag] / 1024) : '';
+        case 'Height': case 'Width': case 'ApertureFNumber':
+            return isset($exif['COMPUTED'][$tag]) ? $exif['COMPUTED'][$tag] : '';
+        case 'Make': case 'Model':
+            return isset($exif[$tag]) ? ucwords(strtolower($exif[$tag])) : '';
+        case 'FocalLength':
+            $length = '';
+            if (isset($exif[$tag])) {
+                $length = $exif[$tag];
+                if (strpos($exif[$tag], '/')) {
+                    $parts = explode('/', $length);
+                    if (count($parts) == 2) {
+                        $length = $parts[0] / $parts[1];
                     }
                 }
-                return $length;
-            case 'ExposureTime':
-                $val = '';
-                if (isset($exif[$tag]))
-                {
-                    if (strstr($exif[$tag], '/'))
-                    {
-                        // Reduce the fraction
-                        $val_parts = explode('/', $exif[$tag]);
-                        if ($val_parts[0] >= $val_parts[1])
-                        {
-                           // Longer than 1 second
-                           $val = $val_parts[0] / $val_parts[1];
-                        }
-                        else
-                        {
-                            // Less than one second
-                            $val = '1/' . ($val_parts[1] / $val_parts[0]);
-                        }
-                    }
-                    elseif ($exif[$tag] < 1)
-                    {
-                        // Turn the decimal into a fraction
-                        $val = '1/' . (1 / $exif[$tag]);
+            }
+            return $length;
+        case 'ExposureTime':
+            $val = '';
+            if (isset($exif[$tag])) {
+                if (strstr($exif[$tag], '/')) {
+                    // Reduce the fraction
+                    $val_parts = explode('/', $exif[$tag]);
+                    if ($val_parts[0] >= $val_parts[1]) {
+                        // Longer than 1 second
+                        $val = $val_parts[0] / $val_parts[1];
                     }
                     else
                     {
-                        $val = $exif[$tag];
+                        // Less than one second
+                        $val = '1/' . ($val_parts[1] / $val_parts[0]);
                     }
                 }
-                return $val;
-            case 'DateTime':
-                $format = ee()->TMPL->fetch_param('format');
-                if (isset($exif['DateTimeOriginal']))
-                {
-                    $date = strtotime($exif['DateTimeOriginal']);
-                }
-                elseif (isset($exif['DateTime']))
-                {
-                    $date = strtotime($exif['DateTime']);
+                elseif ($exif[$tag] < 1) {
+                    // Turn the decimal into a fraction
+                    $val = '1/' . (1 / $exif[$tag]);
                 }
                 else
                 {
-                    return '';
+                    $val = $exif[$tag];
                 }
-                $date += ee()->config->item('vz_exif_offset');
-                return $format ? ee()->localize->decode_date($format, $date) : $date;
-            case 'Flash':
-                return (!@empty($exif['Flash']) && substr(decbin($exif['Flash']), -1) == 1) ? 'Yes' : '';
-            case 'GPSLatitude': case 'GPSLongitude':
-                return isset($exif[$tag]) && isset($exif[$tag.'Ref']) ? $this->parse_geo($exif[$tag], $exif[$tag.'Ref']) : '';
-            default:
-                return isset($exif[$tag]) ? $exif[$tag] : '';
+            }
+            return $val;
+        case 'DateTime':
+            $format = ee()->TMPL->fetch_param('format');
+            if (isset($exif['DateTimeOriginal'])) {
+                $date = strtotime($exif['DateTimeOriginal']);
+            }
+            elseif (isset($exif['DateTime'])) {
+                $date = strtotime($exif['DateTime']);
+            }
+            else
+            {
+                return '';
+            }
+            $date += ee()->config->item('vz_exif_offset');
+            return $format ? ee()->localize->decode_date($format, $date) : $date;
+        case 'Flash':
+            return (!@empty($exif['Flash']) && substr(decbin($exif['Flash']), -1) == 1) ? 'Yes' : '';
+        case 'GPSLatitude': case 'GPSLongitude':
+            return isset($exif[$tag]) && isset($exif[$tag.'Ref']) ? $this->parse_geo($exif[$tag], $exif[$tag.'Ref']) : '';
+        default:
+            return isset($exif[$tag]) ? $exif[$tag] : '';
         }
     }
 
@@ -250,8 +253,7 @@ class Vz_exif {
             $elements[] = $parts[0] / $parts[1];
         }
         $decimal = $elements[0] + ($elements[1] / 60) + ($elements[2] / 3600);
-        if ($ref == 'W' || $ref == 'S')
-        {
+        if ($ref == 'W' || $ref == 'S') {
             $decimal *= -1;
         }
         return $decimal;
